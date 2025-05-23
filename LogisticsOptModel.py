b9fc0ab18607c61b45845321fac2cbae73e66d78
JOHN SHIVOGO <jonesjorney@gmail.com>
2025-05-23 10:07:14 +0300

    Update LogisticsOptModel.py

--- LogisticsOptModel.py
+++ LogisticsOptModel.py
@@ -84,7 +84,7 @@ def add_background():
     st.markdown(f"""
         <style>
             .stApp {{
-                background-image: url("https://raw.githubusercontent.com/SHIVOGOJOHN/Research-Paper-Tool/main/static/images/background1.jpg");
+                background-image: url("https://raw.githubusercontent.com/SHIVOGOJOHN/Research-Paper-Tool/main/static/images/background5.jpg");
                 background-size: cover;
                 background-position: center;
                 background-repeat: no-repeat;
