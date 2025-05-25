2e57f51ae979ba113888d9c48bacd97fda0057aa
JOHN SHIVOGO <jonesjorney@gmail.com>
2025-05-25 22:09:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ def display_home():
     # Display papers
     papers = load_papers()
     for idx, paper in enumerate(papers):
-        col1, col2, col3, col4, col5, col6 = st.columns([2, 2, 1, 2, 1, 1])
+        col1, col2, col3, col4, col5, col6 = st.columns([2, 2, 2, 2, 2, 2])
 
         with col1:
             try:
